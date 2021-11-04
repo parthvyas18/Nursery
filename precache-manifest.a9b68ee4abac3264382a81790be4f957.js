997ec44ef631ac1cacc4ac2a8379c590d141e6ec
PARTH VYAS <parthvyas@PARTHs-MacBook-Pro.local>
2021-11-04 15:40:07 +0530

    Updates

---
+++ precache-manifest.a9b68ee4abac3264382a81790be4f957.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4cc8dc8a3a98d0fd1fb6fb8d431fdf",
+    "url": "/Nursery/index.html"
+  },
+  {
+    "revision": "c1ae2b8f252b86fd1c56",
+    "url": "/Nursery/static/css/2.46ded9fe.chunk.css"
+  },
+  {
+    "revision": "8e1bc95f2943625af856",
+    "url": "/Nursery/static/css/main.c17e49f6.chunk.css"
+  },
+  {
+    "revision": "c1ae2b8f252b86fd1c56",
+    "url": "/Nursery/static/js/2.ab8fc9f1.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/Nursery/static/js/2.ab8fc9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abda154a3ce935dbcda4",
+    "url": "/Nursery/static/js/3.e5afa64a.chunk.js"
+  },
+  {
+    "revision": "8e1bc95f2943625af856",
+    "url": "/Nursery/static/js/main.413a443f.chunk.js"
+  },
+  {
+    "revision": "a5c53ed30c0f20dee51b",
+    "url": "/Nursery/static/js/runtime-main.01de416d.js"
+  },
+  {
+    "revision": "9ab740eef1e1e8c049dd17dc2bd961d7",
+    "url": "/Nursery/static/media/Adenium.9ab740ee.jpeg"
+  },
+  {
+    "revision": "bdf6739ff6dbf4bf9f0ca82d9bcec96d",
+    "url": "/Nursery/static/media/Adenium1.bdf6739f.jpeg"
+  },
+  {
+    "revision": "30282ba75b29941e9218cb7db29ad018",
+    "url": "/Nursery/static/media/Adenium2.30282ba7.jpeg"
+  },
+  {
+    "revision": "af601bde72adc46af8a562cc62498e60",
+    "url": "/Nursery/static/media/Adenium3.af601bde.jpeg"
+  },
+  {
+    "revision": "d1643d6fb38ecfacd539ccfb3271f9af",
+    "url": "/Nursery/static/media/Adenium4.d1643d6f.jpeg"
+  },
+  {
+    "revision": "81794041a00407b0e26a65e4aafd74e9",
+    "url": "/Nursery/static/media/Adenium5.81794041.jpeg"
+  },
+  {
+    "revision": "0ff563e8768b99a62b7cf3aef517107e",
+    "url": "/Nursery/static/media/Cactus1.0ff563e8.jpeg"
+  },
+  {
+    "revision": "f131aae571e6f3e1f28b3063894a1320",
+    "url": "/Nursery/static/media/Cactus2.f131aae5.jpeg"
+  },
+  {
+    "revision": "1d8b15aff8aaf364cf8740dc50e02e14",
+    "url": "/Nursery/static/media/Cactus3.1d8b15af.jpeg"
+  },
+  {
+    "revision": "72d24027680c647a88f0f04ac506c403",
+    "url": "/Nursery/static/media/Cactus4.72d24027.jpeg"
+  },
+  {
+    "revision": "286fee00974668d280e540ac80398a40",
+    "url": "/Nursery/static/media/Cactus5.286fee00.jpeg"
+  },
+  {
+    "revision": "81470d25bb1f9e2130f588c8e97c88c8",
+    "url": "/Nursery/static/media/Cactus6.81470d25.jpeg"
+  },
+  {
+    "revision": "4f8d8bf861f935cc031e38a10c758eb6",
+    "url": "/Nursery/static/media/Cactus7.4f8d8bf8.jpeg"
+  },
+  {
+    "revision": "9c44711a375b8bbf9bbe1dca5b568e8f",
+    "url": "/Nursery/static/media/Cactus8.9c44711a.jpeg"
+  },
+  {
+    "revision": "843c664e78ecc952249b28da39ccfb0c",
+    "url": "/Nursery/static/media/Cactus9.843c664e.jpeg"
+  },
+  {
+    "revision": "b7d3a4925ce541cbc3433d302e7314e9",
+    "url": "/Nursery/static/media/LS1.b7d3a492.jpeg"
+  },
+  {
+    "revision": "627354f8619eb11bd09c479ec37f5094",
+    "url": "/Nursery/static/media/LS10.627354f8.jpeg"
+  },
+  {
+    "revision": "a88a74e46f298b78c00538b4fd9edddc",
+    "url": "/Nursery/static/media/LS2.a88a74e4.jpeg"
+  },
+  {
+    "revision": "24af042de6377dfd968887138f709e07",
+    "url": "/Nursery/static/media/LS3.24af042d.jpeg"
+  },
+  {
+    "revision": "dd5739a153056b96f87d59e5cb9dda98",
+    "url": "/Nursery/static/media/LS4.dd5739a1.jpeg"
+  },
+  {
+    "revision": "a166484bd75bfe85f7a2d43f01a95fce",
+    "url": "/Nursery/static/media/LS5.a166484b.jpeg"
+  },
+  {
+    "revision": "ffe8d7ec068f638293c903d3da375d4c",
+    "url": "/Nursery/static/media/LS6.ffe8d7ec.jpeg"
+  },
+  {
+    "revision": "f3863138bb8944f91ce5da686090d561",
+    "url": "/Nursery/static/media/LS7.f3863138.jpeg"
+  },
+  {
+    "revision": "b8b01e22684219c78ae5b32b2400196c",
+    "url": "/Nursery/static/media/LS8.b8b01e22.jpeg"
+  },
+  {
+    "revision": "46bb84b595b95a2f0496d8ea40bcf113",
+    "url": "/Nursery/static/media/LS9.46bb84b5.jpeg"
+  },
+  {
+    "revision": "63e9f9b14ff3b394541bfd4a7e35a1af",
+    "url": "/Nursery/static/media/Plant1.63e9f9b1.jpeg"
+  },
+  {
+    "revision": "16eaee19bb03d6f7f6489ec3c2896c5f",
+    "url": "/Nursery/static/media/Plant2.16eaee19.jpeg"
+  },
+  {
+    "revision": "a861ac9b0ca1f48e70344b5af868a42c",
+    "url": "/Nursery/static/media/Plant3.a861ac9b.jpeg"
+  },
+  {
+    "revision": "6a4d2810e9ffba5b59bfd1068d963e0a",
+    "url": "/Nursery/static/media/Plant4.6a4d2810.jpeg"
+  },
+  {
+    "revision": "676b10ec97c98bc349d53437f4405eeb",
+    "url": "/Nursery/static/media/astrophycus.676b10ec.jpeg"
+  },
+  {
+    "revision": "d31908e4e0ff68feb3460513556cca9e",
+    "url": "/Nursery/static/media/cactus.d31908e4.jpeg"
+  },
+  {
+    "revision": "80fd17b6c6c18139260ff5a01e978494",
+    "url": "/Nursery/static/media/flowering.80fd17b6.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Nursery/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Nursery/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Nursery/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Nursery/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Nursery/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
